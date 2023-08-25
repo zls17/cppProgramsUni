82246bad00b7566db01b086aa6af4bb44f8b6d81
Ashish Balsure <ashishbalsure@gmail.com>
2023-08-25 21:07:35 +0530

    new images

--- cppPrograms/Conditionals and Loops/multiplesUsingLoops.cpp
+++ cppPrograms/Conditionals and Loops/multiplesUsingLoops.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
 
-int main() {
-    int number {};
-    int howMany {};
+int main()
+{
+    int number{};
+    int howMany{};
     std::cout << "Enter a number: ";
     std::cin >> number;
     std::cout << "How many multiples do you want: ";
     std::cin >> howMany;
-    int counter {};
+    int counter{};
     std::cout << "\nUsing While Loop\n\n";
-    while (counter < (howMany+1)) {
-        std::cout << number << " x " << counter << " = " << number * counter << '\n'; 
+    while (counter < (howMany + 1))
+    {
+        std::cout << number << " x " << counter << " = " << number * counter << '\n';
         ++counter;
     }
     std::cout << "\nUsing For Loop\n\n";
-    for(int i = 0; i < (howMany+1); ++i) {
+    for (int i = 0; i < (howMany + 1); ++i)
+    {
         std::cout << number << " x " << i << " = " << number * i << '\n';
     }
-    
-    int doCounter {};
+
+    int doCounter{};
     std::cout << "\nUsing do-while loop\n\n";
-    do {
+    do
+    {
         std::cout << number << " x " << doCounter << " = " << number * doCounter << '\n';
         ++doCounter;
     } while (doCounter < (howMany + 1));
-
 
     return 0;
 }
