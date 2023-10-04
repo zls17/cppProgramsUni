b942b25bbaa4ca3c0ae54585b6d5469226e524cb
zls <84721045+zls17@users.noreply.github.com>
2023-10-04 21:42:21 +0530

    Update classesIntro.cpp

--- cppPrograms/classes/classesIntro.cpp
+++ cppPrograms/classes/classesIntro.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <iostream>
 
 class CarDealership {
     public:
         std::string model {};
         std::string manufacturer {};
         int yearManufactured {};
-        CarDealership(std::string model, std::string manufacturer, int year) {
-            model = model;
-            manufacturer = manufacturer;
-            yearManufactured = year;
+        CarDealership(std::string mod, std::string manuf, int yr) {
+            model = mod;
+            manufacturer = manuf;
+            yearManufactured = yr;
         }
         void getDetails(CarDealership car) {
             std::cout << "Model of car: " << car.model << "\nManufacturer: " << car.manufacturer 
-            << "\nYear of Manufacturing: " << car.yearManufactured; 
+            << "\nYear of Manufacturing: " << car.yearManufactured << '\n';
         }
 };
 
